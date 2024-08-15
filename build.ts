1936bd567831419cd3dd7331aeab0fa4e6932724
ShootingStarDragons <ShootingStarDragons@protonmail.com>
2024-08-15 17:29:11 +0859

    chore: update

--- build.ts
+++ build.ts
@@ -3,36 +3,60 @@ import { denoPlugins } from "esbuild_deno_loader";
 import { resolve } from "@std/path";
 import { ensureDir, existsSync } from "@std/fs";
 
-const distDir = "dist";
+import { parseArgs } from "@std/cli";
+import { toSSG } from "hono/deno";
 
-ensureDir(distDir);
+import { app } from "./src/index.tsx";
 
-const esBuildOptions: esbuild.BuildOptions = {
-  entryPoints: [
-    "src/aws.ts",
-  ],
-  outdir: distDir,
-  bundle: true,
-  format: "esm",
-  logLevel: "verbose",
-  plugins: [],
-};
-
-// Build Deno Plugin Options
-let importMapURL: string | undefined = resolve("./import_map.json");
-
-if (!existsSync(importMapURL)) {
-  importMapURL = undefined;
+interface ArgsParse {
+  ssg?: boolean;
+  aws?: boolean;
 }
-const configUrl = resolve("./deno.json");
 
-esBuildOptions.plugins = [
-  ...denoPlugins(
-    {
-      importMapURL: importMapURL,
-      configPath: configUrl,
-    },
-  ),
-];
+const input_args = parseArgs(Deno.args) as ArgsParse;
 
-await esbuild.build({ ...esBuildOptions });
+async function buildAws() {
+  const distDir = "dist/aws";
+  ensureDir(distDir);
+  const esBuildOptions: esbuild.BuildOptions = {
+    entryPoints: [
+      "src/aws.ts",
+    ],
+    outdir: distDir,
+    bundle: true,
+    format: "esm",
+    logLevel: "verbose",
+    plugins: [],
+  };
+
+  // Build Deno Plugin Options
+  let importMapURL: string | undefined = resolve("./import_map.json");
+
+  if (!existsSync(importMapURL)) {
+    importMapURL = undefined;
+  }
+  const configUrl = resolve("./deno.json");
+
+  esBuildOptions.plugins = [
+    ...denoPlugins(
+      {
+        importMapURL: importMapURL,
+        configPath: configUrl,
+      },
+    ),
+  ];
+
+  await esbuild.build({ ...esBuildOptions });
+}
+
+function buildSSG() {
+  const dir = "dist/ssg";
+  ensureDir(dir);
+  toSSG(app, { dir });
+}
+
+if (input_args.aws) {
+  await buildAws();
+} else if (input_args.ssg) {
+  buildSSG();
+}
