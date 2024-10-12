0ec3fc42125ed94b407d57142febfb625fd6ab43
Decodetalkers <ShootingStarDragons@protonmail.com>
2024-10-12 00:54:16 +0000

    Deploying to gh-pages from @ neocmakelsp/neocmakelsp.github.io@2a8b70f0156610c25a59ee68e828c0320238d6f9 🚀

--- doc/doc.js
+++ doc/doc.js
@@ -6222,8 +6222,8 @@ var MarkdownArea = mod_default.div`
   overflow-y: scroll;
   overflow-x: hidden;
   overflow-wrap: break-word;
-  padding-left: ${({ isOpen }) => isOpen ? "170px" : "40px"};
-  padding-right: 5%;
+  padding-left: ${({ isOpen }) => isOpen ? "170px" : "50px"};
+  padding-right: 40px;
   padding-top: 80px;
   padding-bottom: 80px;
   transition: padding-left 0.3s ease-in-out;
