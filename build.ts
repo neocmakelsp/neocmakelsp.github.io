0917f44c68a9614c237abf284675a0317d1e271e
ShootingStarDragons <ShootingStarDragons@protonmail.com>
2024-10-12 23:48:02 +0900

    feat: better hotreload

--- build.ts
+++ build.ts
@@ -1,9 +1,14 @@
 import { serveDir } from "@std/http";
-import { delay } from "@std/async";
-
 import { parseArgs } from "@std/cli";
 
-import { GenWebsite, Route, type Script, WebPageUnit } from "@nobody/tananoni";
+import {
+  GenWebsite,
+  refreshMiddleware,
+  Route,
+  type Script,
+  watchChanges,
+  WebPageUnit,
+} from "@nobody/tananoni";
 
 interface BuildMode {
   debug?: boolean;
@@ -23,46 +28,7 @@ if (release_mode) {
 const base_asserts = { path: "static/asserts", alias: "static" };
 const css_asserts = { path: "static/styles" };
 
-/**
- * In-memory store of open WebSockets for
- * triggering browser refresh.
- */
-const sockets: Set<WebSocket> = new Set();
-
-/**
- * Upgrade a request connection to a WebSocket if
- * the url ends with "/refresh"
- */
-function refreshMiddleware(req: Request): Response | null {
-  if (req.url.endsWith("/refresh")) {
-    const { response, socket } = Deno.upgradeWebSocket(req);
-
-    // Add the new socket to our in-memory store
-    // of WebSockets.
-    sockets.add(socket);
-
-    // Remove the socket from our in-memory store
-    // when the socket closes.
-    socket.onclose = () => {
-      sockets.delete(socket);
-    };
-
-    return response;
-  }
-
-  return null;
-}
-
 const scripts: Script[] = [{ src: "main.js" }];
-
-if (!release_mode) {
-  scripts.push({ src: "./refresh/client.js" });
-}
-
-const docscripts: Script[] = [];
-if (!release_mode) {
-  docscripts.push({ src: "./refresh/client.js" });
-}
 
 const docroutine = new Route("doc")
   .append_assert(base_asserts)
@@ -71,45 +37,59 @@ const docroutine = new Route("doc")
     new WebPageUnit(
       "./src/doc/introduction.tsx",
       [{ type: "header", id: "header" }, { type: "main", id: "mount" }],
-      [{ type: "module", src: "introduction.js" }, ...docscripts],
+      [{ type: "module", src: "introduction.js" }],
     )
       .with_title("neocmakelsp introduction").with_linkInfos([
         { type: "stylesheet", href: "styles/global.css" },
         { type: "icon", href: "favicon.ico" },
-      ]),
+      ])
+      .then((webpage) => {
+        if (!release_mode) {
+          webpage.with_hotReload();
+        }
+        return webpage;
+      }),
   )
   .append_webpage(
     new WebPageUnit(
       "./src/doc/installdoc.tsx",
       [{ type: "header", id: "header" }, { type: "main", id: "mount" }],
-      [{ type: "module", src: "installdoc.js" }, ...docscripts],
+      [{ type: "module", src: "installdoc.js" }],
     )
-      .with_title("How to Install").with_linkInfos([
+      .with_title("How to Install")
+      .with_linkInfos([
         { type: "stylesheet", href: "styles/global.css" },
         { type: "icon", href: "favicon.ico" },
       ])
-      .with_htmlName("install.html"),
+      .with_htmlName("install.html")
+      .then((webpage) => {
+        if (!release_mode) {
+          webpage.with_hotReload();
+        }
+        return webpage;
+      }),
   )
   .append_webpage(
     new WebPageUnit(
       "./src/doc/usagedoc.tsx",
       [{ type: "header", id: "header" }, { type: "main", id: "mount" }],
-      [{ type: "module", src: "usagedoc.js" }, ...docscripts],
+      [{ type: "module", src: "usagedoc.js" }],
     )
       .with_title("How to use").with_linkInfos([
         { type: "stylesheet", href: "styles/global.css" },
         { type: "icon", href: "favicon.ico" },
       ])
-      .with_htmlName("usage.html"),
+      .with_htmlName("usage.html")
+      .then((webpage) => {
+        if (!release_mode) {
+          webpage.with_hotReload();
+        }
+        return webpage;
+      }),
   )
+  .with_hotReload(!release_mode)
   .append_assert({ path: "./markdowns" })
-  .append_assert({ path: "./static/asserts/favicon.ico" })
-  .then((route) => {
-    if (!release_mode) {
-      route = route.append_assert({ path: "./static/debug/refresh" });
-    }
-    return route;
-  });
+  .append_assert({ path: "./static/asserts/favicon.ico" });
 
 const homeroute = new Route(route_path)
   .append_assert(base_asserts)
@@ -127,61 +107,22 @@ const homeroute = new Route(route_path)
       .with_linkInfos([
         { type: "stylesheet", href: "styles/global.css" },
         { type: "icon", href: "static/favicon.ico" },
-      ]),
+      ])
+      .then((webpage) => {
+        if (!release_mode) {
+          webpage.with_hotReload();
+        }
+        return webpage;
+      }),
   )
   .append_route(docroutine)
-  .then((route) => {
-    if (!release_mode) {
-      route = route.append_assert({ path: "./static/debug/refresh" });
-    }
-    return route;
-  });
+  .with_hotReload(!release_mode);
 
 const webgen = new GenWebsite()
   .withLogLevel("info")
   .withImportSource("npm:preact");
 
 await webgen.generate_website(homeroute);
-
-async function watch() {
-  let during_wait = false;
-
-  const watcher = Deno.watchFs("./");
-
-  for await (const event of watcher) {
-    if (during_wait) {
-      continue;
-    }
-    if (["any", "access"].includes(event.kind)) {
-      continue;
-    }
-
-    let should_fresh = false;
-
-    for (const pa of event.paths) {
-      if (
-        pa.includes("./dist") || pa.includes("./build.ts") ||
-        (pa.endsWith(".git") || pa.includes(".git/")) ||
-        (!pa.endsWith("ts") && !pa.endsWith("tsx") && !pa.endsWith("css") &&
-          !pa.endsWith("js") && !pa.endsWith("jsx") && !pa.endsWith("md"))
-      ) {
-        continue;
-      }
-      should_fresh = true;
-      break;
-    }
-    if (!should_fresh) {
-      continue;
-    }
-
-    await webgen.generate_website(homeroute);
-    sockets.forEach((socket) => {
-      socket.send("refresh");
-    });
-    during_wait = true;
-    delay(1000).then(() => during_wait = false);
-  }
-}
 
 if (release_mode) {
   Deno.exit(0);
@@ -198,4 +139,13 @@ Deno.serve({ hostname: "localhost", port: 8000 }, async (req) => {
   return await serveDir(req, { fsRoot });
 });
 
-await watch();
+async function fsWatch() {
+  await webgen.generate_website(homeroute);
+}
+
+await watchChanges({
+  watchedDir: "./",
+  watchedFileTypes: [".ts", ".tsx", ".css", ".md"],
+  excludes: ["dist", "build.ts", "build.ts~"],
+  fallback: fsWatch,
+});
