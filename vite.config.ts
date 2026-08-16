34abdddd1b5e3f86c4f0358324ce2eb95f68d8cf
ShootingStarDragons <ShootingStarDragons@protonmail.com>
2026-08-17 02:12:59 +0900

    Revert "fix: build"
    This reverts commit 7050344d27e359afcf2c8ad960f81c7a82685131.

--- vite.config.ts
+++ vite.config.ts
@@ -25,10 +25,7 @@ export default defineConfig({
         fallback: undefined,
         precompress: false,
         strict: true
-      }),
-      paths: {
-        base: process.argv.includes('dev') ? `/${process.env.BASE_PATH}` : ''
-      }
+      })
     })
   ],
   test: {
