7050344d27e359afcf2c8ad960f81c7a82685131
ShootingStarDragons <ShootingStarDragons@protonmail.com>
2026-08-17 01:22:14 +0900

    fix: build

--- vite.config.ts
+++ vite.config.ts
@@ -25,7 +25,10 @@ export default defineConfig({
         fallback: undefined,
         precompress: false,
         strict: true
-      })
+      }),
+      paths: {
+        base: process.argv.includes('dev') ? `/${process.env.BASE_PATH}` : ''
+      }
     })
   ],
   test: {
